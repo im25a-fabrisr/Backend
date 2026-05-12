6759922f741256ca2a3d427d06adcc57d47ce6d0
im25a-fabrisr <fabrisr@bzz.ch>
2026-05-12 08:59:14 +0200

    models

--- models.py
+++ models.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from database import Base
 
 class Aufgabe(Base):
-    __tablename__ = "aufgaben" # Muss exakt wie in MySQL heißen
+    __tablename__ = "Aufgabe"
 
-    id = Column(Integer, primary_key=True, index=True)
-    titel = Column(String(100))
-    beschreibung = Column(String(255))
-    erledigt = Column(Boolean, default=False)
+    AufgabeID = Column(Integer, primary_key=True, index=True)
+    Titel = Column(String(100))
+    Beginn = Column(DateTime)
+    Ende = Column(DateTime)
+    Ort = Column(String(250))
+    Koordinaten = Column(String(100))
+    Notiz = Column(Text)
+    KategorieID = Column(Integer)
+    PrioritaetID = Column(Integer)
+    FortschrittID = Column(Integer)
+    BenutzerID = Column(Integer)
